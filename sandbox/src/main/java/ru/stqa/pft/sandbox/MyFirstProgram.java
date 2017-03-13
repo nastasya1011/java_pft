059c17ad2f1d84941bb970e3ee6eee110cbc3bf6
aanosova <nastasya1011@gmail.com>
2017-03-13 15:42:44 +0300

    Расстояние между точками на двумерной плоскости.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,19 +3,30 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
   public static void main(String[] args) {
 
-    hello("world");
-    hello("user");
-    hello("Nastya");
+    Point p1 = new Point (2.0, 8.0);
+    Point p2 = new Point (3.0, 7.0);
 
-    Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+    System.out.println("Расстояние между точками " + p1 + " и " + p2 + " = " + distance(p1,p2) );
 
-    Rectangle r = new Rectangle(4,6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    //hello("Nastya");
+
+    //Square s = new Square(5);
+   // System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+   // Rectangle r = new Rectangle(4,6);
+    //System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
   }
 
-  public static void hello(String somebody) {
-    System.out.println("Hello," + somebody + "!");
+  //public static void hello(String somebody) {
+  // System.out.println("Hello," + somebody + "!");
+  //}
+
+  public static double distance (Point p1, Point p2) {
+    double diffX = p1.x - p2.x;
+    double diffY = p1.y - p2.y;
+
+    return Math.sqrt (Math.pow(diffX, 2) + Math.pow(diffY, 2));
   }
 
 

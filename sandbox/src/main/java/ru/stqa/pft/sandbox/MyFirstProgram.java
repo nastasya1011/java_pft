9f5298cce4c600adde72ca62fbf80555b54ca4da
aanosova <nastasya1011@gmail.com>
2017-04-13 17:11:17 +0300

    Создание новой группы в адресной книге

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,8 +3,8 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
   public static void main(String[] args) {
 
-    Point p1 = new Point (2.0, 8.0);
-    Point p2 = new Point (3.0, 7.0);
+    Point p1 = new Point (0.0, 0.0);
+    Point p2 = new Point (4.0, 4.0);
 
     System.out.println("Расстояние между точками " + p1 + " и " + p2 + " = " + distance(p1,p2) );
 
